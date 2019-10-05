cde00d302dafea3adee3f83962517255f8f336d3
Super Apollo <apollo@Supers-iMac.local>
2019-10-05 16:12:12 +0200

    update api

---
+++ app/controllers/api/v1/contacts_controller.rb
@@ -0,0 +1,32 @@
+module Api
+  module V1
+    class ContactsController < ApplicationController
+      def index
+
+        render json: { 
+          data: [{
+            key: '1',
+            name: 'John Brown',
+            age: 32,
+            address: 'New York No. 1 Lake Park',
+            tags: ['nice', 'developer'],
+          },
+          {
+            key: '2',
+            name: 'Jim Green',
+            age: 42,
+            address: 'London No. 1 Lake Park',
+            tags: ['loser'],
+          },
+          {
+            key: '3',
+            name: 'Joe Black',
+            age: 32,
+            address: 'Sidney No. 1 Lake Park',
+            tags: ['cool', 'teacher'],
+          }],
+        }, status: :ok
+      end
+    end
+  end
+end
